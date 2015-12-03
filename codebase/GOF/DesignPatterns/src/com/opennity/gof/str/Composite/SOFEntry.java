ab4477bcaef04421959b1dd96714d7b49f92d8c7
pashabhai <pashabhai@gmail.com>
2015-12-03 16:11:11 +0530

    composite pattern working example

--- codebase/GOF/DesignPatterns/src/com/opennity/gof/str/Composite/SOFEntry.java
+++ codebase/GOF/DesignPatterns/src/com/opennity/gof/str/Composite/SOFEntry.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  *
  * @author Prashant.Purkar
- * This class represents the entry on a stackoverflow.com web site.
+ * This class represents the entry/blog on a stackoverflow.com web site.
  * entry can be of type question, answer and a comment
  *
  *  voteUp, VoteDown are methods that clients of the components can call.
@@ -13,11 +13,26 @@ import java.util.List;
 
 public abstract class SOFEntry {
 
+	protected int upVotes = 0;
+	protected int dwonVotes = 0;
+	protected String value;
+
+	public SOFEntry(String text){
+		this.value = text;
+	}
+
 	public abstract void voteUp();
 	public abstract void voteDown();
-	public abstract void add(SOFEntry entry);
-	public abstract void remove(SOFEntry enty);
+	public abstract String edit();
+	public abstract String publish();
+	public abstract boolean add(SOFEntry entry);
+	public abstract boolean remove(SOFEntry entry);
 	public abstract List<SOFEntry> getEntries();
+
+	@Override
+	public String toString() {
+		return value;
+	}
 
 
 }
